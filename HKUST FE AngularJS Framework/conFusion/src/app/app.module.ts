cbb58f07bd5537a38ffc8b319b33392f23f2170c
allendomingo <allen.domingo@eee.upd.edu.ph>
2021-11-16 23:26:58 +0800

    HKUST-AngularJs Module 1: Components Part 1 - Add menu of dishes

--- HKUST FE AngularJS Framework/conFusion/src/app/app.module.ts
+++ HKUST FE AngularJS Framework/conFusion/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     FlexLayoutModule,
+    MatListModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
