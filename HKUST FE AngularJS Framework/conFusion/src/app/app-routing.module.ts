4caeb12287c5a5c8e6ada31e839bb1c27919d42e
allendomingo <allen.domingo@eee.upd.edu.ph>
2021-11-23 19:35:06 +0800

    HKUST-AngularJs Module 2: Set routes to apprpriate components, and link said routes to both header and footer buttons/links

--- HKUST FE AngularJS Framework/conFusion/src/app/app-routing.module.ts
+++ HKUST FE AngularJS Framework/conFusion/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
