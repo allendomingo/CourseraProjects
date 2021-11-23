8adffec6b812f43d2bb7b23a92e5b3cc15f6a042
allendomingo <allen.domingo@eee.upd.edu.ph>
2021-11-23 19:26:46 +0800

    HKUST-AngularJs Module 2: Generate home, about, and contact components

--- HKUST FE AngularJS Framework/conFusion/src/app/app.module.ts
+++ HKUST FE AngularJS Framework/conFusion/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +29,9 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
