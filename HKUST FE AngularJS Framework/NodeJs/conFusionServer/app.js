f0cdd49e5081b9d8e71a7e3211956ac6c6f118b9
allendomingo <allen.domingo@eee.upd.edu.ph>
2021-12-15 16:44:59 +0800

    Module 2: Clean up app.js file

--- HKUST FE AngularJS Framework/NodeJs/conFusionServer/app.js
+++ HKUST FE AngularJS Framework/NodeJs/conFusionServer/app.js
@@ -15,7 +15,7 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
+connect.then(() => {
 	console.log('Connected correctly to server');
 }, (err) => { console.log(err) });
 
